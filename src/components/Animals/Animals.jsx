c8cf456b1156940abe5dc8e1dca291ea4615d732
Arseniy <arseniy.sinyav@gmail.com>
2023-05-28 15:11:48 +0300

    Animal: add onClick -> navigate to animal/id

--- src/components/Animals/Animals.jsx
+++ src/components/Animals/Animals.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import {
   Typography,
   Card,
@@ -5,19 +6,25 @@ import {
   Paper,
   CardMedia,
 } from "@mui/material";
+import { useNavigate } from "react-router";
 
 export const Animals = ({ animals }) => {
+  const navigate = useNavigate();
+
   return (
     <div>
-      {animals.map((animals) => {
+      {animals.map((animal) => {
         return (
-          <div className="p-12">
+          <div
+            className="p-12"
+            onClick={() => navigate(`/animal/${animal.id}`)}
+          >
             <Card className="flex shadow" sx={{ height: 350 }}>
               <CardActionArea className="flex flex-col">
                 <CardMedia
                   component="img"
-                  src={`http://miomi.by:9000${animals.photos[0]}`}
-                  alt={animals.name}
+                  src={`http://miomi.by:9000${animal.photos[0]}`}
+                  alt={animal.name}
                 />
                 <Paper
                   sx={{
@@ -31,7 +38,7 @@ export const Animals = ({ animals }) => {
                     p: 1,
                   }}
                 >
-                  <Typography className="pl-6">{animals.name}</Typography>
+                  <Typography className="pl-6">{animal.name}</Typography>
                 </Paper>
                 <div className="w-full px-16 pt-16"></div>
               </CardActionArea>
