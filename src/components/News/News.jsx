7886c49d18cfef0ad781a16c3da3166a07e32bdc
quaesum <pozaritskiy0212@gmail.com>
2023-05-30 09:53:30 +0300

    News added

--- src/components/News/News.jsx
+++ src/components/News/News.jsx
@@ -1,14 +1,33 @@
-import { Typography } from "@mui/material";
+import { CardActionArea, CardContent, CardMedia, Typography, Card } from "@mui/material";
 import React from "react";
+import { useNavigate } from "react-router";
 
 export const News = ({ news }) => {
+  const navigate = useNavigate();
   return (
-    <div>
+    <div className="grid grid-col-1">
       {news.map((n) => {
         return (
-          <div>
-            <Typography key={n.id}>{n.label}</Typography>
-          </div>
+          <Card className='flex shadow m-10' >
+            <CardActionArea onClick={() => navigate(`/news/${n.id}`)}>
+              <div className="flex flex-col items-center md:flex-row ">
+                <CardContent className="flex flex-col">
+                  <CardMedia
+                    src={`http://miomi.by:9000${n.photo}`}
+                    component="img"
+                    alt={n.id}
+                    className="rounded-lg ml-6"
+                    sx={{width: 150, height: 150}}
+                  />
+                </CardContent>
+                <CardContent className="flex flex-col w-full">
+                  <Typography fontSize={24}>{n.label}</Typography>
+                  <Typography>{n.created_at}</Typography>
+                </CardContent>
+              </div>
+            </CardActionArea>
+          </Card>
+
         );
       })}
     </div>
