5dd51ac80a32b3d619e5313b37f60d9c4beb4ca1
Arseniy <arseniy.sinyav@gmail.com>
2023-06-07 22:21:50 +0300

    Profile: if not login  navigate('/')

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,13 @@ import React from "react";
 import { Navigate, Route, Routes } from "react-router";
 import { CurrentAnimalPageContainer } from "./pages/CurrentAnimalPage/CurrentAnimalPageContainer";
 import Layout from "./components/Layout/Layout";
-import { LoginContainer } from "./components/Login/LoginContainer";
 import { MainPage } from "./pages/MainPage/MainPage";
 import TermsOfUse from "./pages/TermsOfUse";
 import ProfilePageContainer from "./pages/ProfilePage/ProfilePageContainer.";
 import { CurrentNewsPageContainer } from "./pages/CurrentNewsPage/CurrentNewsPageContainer";
 import { CreateAnimalPage } from "./pages/CreateAnimalPage/CreateAnimalPage";
 import { RegistrationContainer } from "./pages/Registration/RegistrationContainer";
+import { LoginContainer } from "./pages/Login/LoginContainer";
 
 export const App = () => {
   return (
--- src/pages/Login/LoginContainer.jsx
+++ src/pages/Login/LoginContainer.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Login } from "./Login";
-import { useAnimalContext } from "../../Context/AnimalContext";
 import { useNavigate } from "react-router";
+import { useAnimalContext } from "../../Context/AnimalContext";
 
 export const LoginContainer = () => {
   const { login, isLogin, setIsLogin } = useAnimalContext();
