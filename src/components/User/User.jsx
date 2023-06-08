025ced839a5e9d5a4e7040f558905c9d6a7c6398
Arseniy <arseniy.sinyav@gmail.com>
2023-06-08 14:57:14 +0300

    Fix user name

--- src/components/User/User.jsx
+++ src/components/User/User.jsx
@@ -2,16 +2,33 @@ import { Avatar, Box, Typography } from "@mui/material";
 import React from "react";
 
 export const User = ({ firstName, lastName, isAllAnimal, setIsAllAnimal }) => {
-  const userName = `${firstName} ${lastName}`;
+  const userName = ` `;
 
   return (
     <>
-      <Box sx={{ p: {md: "25px", xs: "5px"}, mx: "10px" }} className="flex justify-between">
+      <Box
+        sx={{ p: { md: "25px", xs: "5px" }, mx: "10px" }}
+        className="flex justify-between"
+      >
         <Box className="flex flex-row items-center">
-          <Avatar sx={{ width: {lg:100, md:70, xs: 60}, height: {lg:100, md:70, xs: 60} }}></Avatar>
+          <Avatar
+            sx={{
+              width: { lg: 100, md: 70, xs: 60 },
+              height: { lg: 100, md: 70, xs: 60 },
+            }}
+          ></Avatar>
           <Box className="flex flex-col ml-10">
-            <Typography fontWeight={600} sx={{fontSize: {md: "1.5rem", xs: "1rem"}}}>
-              {userName}
+            <Typography
+              fontWeight={600}
+              sx={{ fontSize: { md: "1.5rem", xs: "1rem" } }}
+            >
+              {firstName}
+            </Typography>
+            <Typography
+              fontWeight={600}
+              sx={{ fontSize: { md: "1.5rem", xs: "1rem" } }}
+            >
+              {lastName}
             </Typography>
           </Box>
         </Box>
@@ -25,7 +42,7 @@ export const User = ({ firstName, lastName, isAllAnimal, setIsAllAnimal }) => {
                 pr: "15px",
                 borderRadius: "10px",
                 cursor: "pointer",
-                fontSize: {xs: "15px", md: "20px"},
+                fontSize: { xs: "15px", md: "20px" },
                 boxShadow:
                   "0px 3px 1px -2px rgba(0,0,0,0.2), 0px 2px 2px 0px rgba(0,0,0,0.14), 0px 1px 5px 0px rgba(0,0,0,0.12)",
                 transition: "0.2s",
@@ -50,7 +67,7 @@ export const User = ({ firstName, lastName, isAllAnimal, setIsAllAnimal }) => {
                       backgroundColor: "#DCDCDC",
                       border: "1px solid #DCDCDC",
                       boxShadow:
-                      "0px 2px 4px -1px rgba(0,0,0,0.2), 0px 4px 5px 0px rgba(0,0,0,0.14), 0px 1px 10px 0px rgba(0,0,0,0.12) !important",
+                        "0px 2px 4px -1px rgba(0,0,0,0.2), 0px 4px 5px 0px rgba(0,0,0,0.14), 0px 1px 10px 0px rgba(0,0,0,0.12) !important",
                     },
                   },
             ]}
